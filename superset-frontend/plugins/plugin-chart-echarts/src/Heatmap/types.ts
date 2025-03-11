1ac1dd2b4bbf7c67eca833e4663c752aa6b60f45
Devanjan Banerjee <92582883+Dev10-34@users.noreply.github.com>
2025-03-11 18:57:30 +0000

    modified:   superset-frontend/plugins/plugin-chart-echarts/src/Heatmap/types.ts

--- superset-frontend/plugins/plugin-chart-echarts/src/Heatmap/types.ts
+++ superset-frontend/plugins/plugin-chart-echarts/src/Heatmap/types.ts
@@ -35,7 +35,7 @@ export interface HeatmapFormData extends QueryFormData {
   normalizeAcross: 'heatmap' | 'x' | 'y';
   normalized?: boolean;
   borderColor: RgbaColor;
-   borderWidth: number;
+  borderWidth: number;
   showLegend?: boolean;
   showPercentage?: boolean;
   showValues?: boolean;
